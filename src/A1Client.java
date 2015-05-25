294c4c2d27f96e063df566d7bdb32287b5e42d5a
jzanutto <jzanutto@live.com>
2015-05-24 21:24:04 -0400

    Implemented test client
    To execute, run 'ant client'
    Test client service with password API
    Need to add management API functionality

--- src/A1Client.java
+++ src/A1Client.java
@@ -1,11 +1,7 @@
-import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TServer.Args;
-import org.apache.thrift.server.TSimpleServer;
-import org.apache.thrift.server.TThreadPoolServer;
-import org.apache.thrift.transport.TSSLTransportFactory;
-import org.apache.thrift.transport.TServerSocket;
-import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.server.*;
+import org.apache.thrift.transport.*;
 import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
+import org.apache.thrift.protocol.*;
 
 import ece454750s15a1.*;
 
@@ -23,6 +19,28 @@ public class A1Client extends Server {
     }
 
     protected void start() {
+        TTransport transport;
+        try {
+            transport = new TSocket(getHost(), getPPort());
+            TProtocol protocol = new TBinaryProtocol(transport);
+            A1Password.Client client = new A1Password.Client(protocol);
 
+            transport.open();
+
+            String passwd = "hunter2";
+            short rounds = 16;
+            System.out.println("password is: " + passwd);
+            String hash = client.hashPassword(passwd, rounds);
+            System.out.println(hash + " is the hashed password");
+            if (client.checkPassword(passwd, hash)) {
+                System.out.println(hash + " is the hashed version of " + passwd);
+            } else {
+                System.out.println(hash + " is NOT the hashed version of " + passwd);
+            }
+
+            transport.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
