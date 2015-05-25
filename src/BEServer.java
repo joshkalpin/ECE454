d11c1edb20458a7455f59f7ce431979165f94696
jzanutto <jzanutto@live.com>
2015-05-25 19:21:16 -0400

    Added comprehensive logging to backend server
    - more build.xml tweaks

--- src/BEServer.java
+++ src/BEServer.java
@@ -5,11 +5,16 @@ import org.apache.thrift.protocol.*;
 import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ece454750s15a1.*;
 
 public class BEServer extends Server {
 
     private DiscoveryInfo info;
+    private Logger logger;
+
 
     public static int DISCOVERY_TIMEOUT = 10000;
 
@@ -25,6 +30,8 @@ public class BEServer extends Server {
     public BEServer(String[] args) {
         super(args);
         info = new DiscoveryInfo(this.getHost(), this.getMPort(), this.getPPort(), true);
+        logger = LoggerFactory.getLogger(BEServer.class);
+        logger.info("Node created...");
     }
 
     @Override
@@ -33,33 +40,33 @@ public class BEServer extends Server {
             TServerTransport passwordTransport = new TServerSocket(this.getPPort());
             A1Password.Processor passwordProcessor = new A1Password.Processor(new A1PasswordHandler());
             TThreadPoolServer.Args passwordArgs = new TThreadPoolServer.Args(passwordTransport);
-            passwordArgs.maxWorkerThreads(getNCores());
+            // passwordArgs.maxWorkerThreads(getNCores());
             final TServer passwordServer =
                 new TThreadPoolServer(passwordArgs.processor(passwordProcessor));
             System.out.println("Starting BE password service " + this.getPPort() + "...");
-
             Runnable passwordHandler = new Runnable() {
                 public void run() {
                     passwordServer.serve();
                 }
             };
 
+            logger.info("Attempting to start password service...");
             new Thread(passwordHandler).start();
 
             TServerTransport managementTransport = new TServerSocket(this.getMPort());
             A1Management.Processor managementProcessor = new A1Management.Processor(new A1ManagementHandler());
             TThreadPoolServer.Args managementArgs = new TThreadPoolServer.Args(managementTransport);
-            managementArgs.maxWorkerThreads(getNCores());
+            // managementArgs.maxWorkerThreads(getNCores());
             final TServer managementServer =
                 new TThreadPoolServer(managementArgs.processor(managementProcessor));
             System.out.println("Starting BE management server " + this.getMPort() + "...");
-
             Runnable managementHandler = new Runnable() {
                 public void run() {
                     managementServer.serve();
                 }
             };
 
+            logger.info("Attempting to start management service...");
             new Thread(managementHandler).start();
 
             List<DiscoveryInfo> seeds = getSeeds();
@@ -75,6 +82,7 @@ public class BEServer extends Server {
 
     private void register(String host, int mPort) {
         try {
+            logger.info("Registering with node " + host + ":" + mPort);
             TSocket transport;
             transport = new TSocket(host, mPort);
             TProtocol protocol = new TBinaryProtocol(transport);
@@ -87,7 +95,7 @@ public class BEServer extends Server {
 
             transport.close();
         } catch (Exception e) {
-            System.out.println("Failed to register with " + host + ":" + mPort);
+            logger.warn("Failed to register with " + host + ":" + mPort);
             e.printStackTrace();
         }
     }
