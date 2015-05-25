3dcc699467750d87322ef05bf5bcb541d958be8f
Joshua Kalpin <joshua.kalpin@gmail.com>
2015-05-25 11:30:03 -0400

    Add completion handlers for async method calls

--- src/A1ManagementHandlerAsync.java
+++ src/A1ManagementHandlerAsync.java
@@ -11,13 +11,15 @@ public class A1ManagementHandlerAsync implements A1Management.AsyncIface {
     public A1ManagementHandlerAsync() {}
 
     public void getPerfCounters(AsyncMethodCallback resultHandler) throws TException {
-
+        resultHandler.onComplete(new PerfCounters());
     }
 
     public void getGroupMembers(AsyncMethodCallback resultHandler) throws TException {
         List<String> members = new ArrayList<String>();
         members.add("jzanutto");
         members.add("jkalpin");
+
+        resultHandler.onComplete(members);
     }
 
 
