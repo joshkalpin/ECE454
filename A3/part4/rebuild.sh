4fc9499000dd1705e2818635a6a58098068e5d02
jzanutto <jzanutto@live.com>
2015-07-22 22:50:37 -0400

    removed my username from the rebuild script Kappa

--- A3/part4/rebuild.sh
+++ A3/part4/rebuild.sh
@@ -6,12 +6,12 @@ jar -cf $2.jar $2*.class
 echo "Cleaning up old outputs and log files"
 rm -rf output/
 mkdir output/
-hdfs dfs -rm -r -skipTrash /user/jzanutto/output/
+hdfs dfs -rm -r -skipTrash /user/$(whoami)/output/
 rm *.log
 echo "Starting pig script"
-pig -param input=/user/jzanutto/input/1k_Samples -param output=/user/jzanutto/output/ $1
+pig -param input=/user/$(whoami)/input/1k_Samples -param output=/user/$(whoami)/output/ $1
 echo "Copying output from HDFS."
-hdfs dfs -copyToLocal /user/jzanutto/output/
+hdfs dfs -copyToLocal /user/$(whoami)/output/
 echo "Sorting output."
 sort output/part-*-00000 > output/part-*-00000.sort
 echo "=================== OUTPUT ==================="
